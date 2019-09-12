ea0154f44f33981fb150ffefbae852babe644523
Paul Trojanowski <trojanowski.dev@gmail.com>
2019-09-12 15:42:47 +0200

    Django >2.0 fixes

--- online_users/middleware.py
+++ online_users/middleware.py
@@ -4,7 +4,6 @@ from online_users.models import OnlineUserActivity
 class OnlineNowMiddleware(object):
     """Updates the OnlineUserActivity database whenever an authenticated user makes an HTTP request."""
 
-    @staticmethod
     def process_request(self, request):
         user = request.user
         if not user.is_authenticated:
