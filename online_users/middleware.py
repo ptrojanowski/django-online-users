54672dc382396172bf0db2a24d5a2a73eec3fd23
Paul Trojanowski <trojanowski.dev@gmail.com>
2019-09-13 08:25:19 +0200

    new style of middleware

--- online_users/middleware.py
+++ online_users/middleware.py
@@ -2,11 +2,14 @@ from online_users.models import OnlineUserActivity
 
 
 class OnlineNowMiddleware(object):
-    """Updates the OnlineUserActivity database whenever an authenticated user makes an HTTP request."""
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-    def process_request(self, request):
+    def __call__(self, request):
         user = request.user
         if not user.is_authenticated:
             return
 
         OnlineUserActivity.update_user_activity(user)
+        response = self.get_response(request)
+        return response
