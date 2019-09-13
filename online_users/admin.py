21435da3c17e48e7e5bae5c1d41e4323507e4a8d
Paul Trojanowski <trojanowski.dev@gmail.com>
2019-09-13 09:20:40 +0200

    fixed search fields

--- online_users/admin.py
+++ online_users/admin.py
@@ -5,10 +5,11 @@ from online_users.models import OnlineUserActivity
 
 class OnlineUserActivityAdmin(admin.ModelAdmin):
     list_display = ('user', 'last_activity',)
-    search_fields = ['user__username', ]
+    search_fields = ['user__name', 'user__email']
     list_filter = ['last_activity']
 
     def get_ordering(self, request):
         return ['last_activity']
+
 
 admin.site.register(OnlineUserActivity, OnlineUserActivityAdmin)
